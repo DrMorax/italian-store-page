5f3e819f93291f7e8c965bb8b004e6e587bf762c
Morax <abbasbusiness01@gmail.com>
2024-07-08 23:37:39 +0300

    idk

---
+++ src/ui/components/hero.tsx
@@ -0,0 +1,12 @@
+export default function Hero() {
+  return (
+    <div className="hero p-8 text-center sm:text-left">
+      <h1 className="mt-16 text-6xl sm:text-7xl">Hello, world</h1>
+      <p className="text-md sm:w-[50%]">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi natus
+        distinctio tempora saepe laborum nihil laudantium dolores magni illo
+        commodi.
+      </p>
+    </div>
+  );
+}
